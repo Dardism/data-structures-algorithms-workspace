6ce782928c82df34ce4fb7c96d1832264a12e16e
Dardism <dardismm@gmail.com>
2018-03-29 15:01:30 -0400

    completed classes for insertion sort program

---
+++ insertionSort/insertionSort/ArrayIns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insertionSort {
+  class ArrayIns {
+    private long[] a;
+    private int nElems; //number of elements
+
+    public ArrayIns(int max) {
+      a = new long[max];
+      nElems = 0;
+    }
+    //***********************************
+    public void insert(long value) {
+      a[nElems] = value;
+      nElems++;
+    }
+    //***********************************
+    public void display() {
+      for (int j = 0; j < nElems; j++) {
+        Console.Write(a[j] + "  ");
+      }
+      Console.WriteLine("");
+    }
+    //***********************************
+    public void insertionSort() {
+      int inner;
+      int outer;
+
+      for (outer = 1; outer < nElems; outer++) {
+        long temp = a[outer];
+        inner = outer;
+        while (inner > 0 && a[inner - 1] >= temp) {
+          a[inner] = a[inner - 1];
+          --inner;
+        }
+        a[inner] = temp;
+      }
+    }
+  }
+}
---
+++ insertionSort/insertionSort/InsertSortApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insertionSort {
+  class InsertSortApp {
+    static void Main(string[] args) {
+      int maxSize = 100; //array size
+      ArrayIns arr = new ArrayIns(maxSize);
+
+      arr.insert(77);
+      arr.insert(99);
+      arr.insert(44);
+      arr.insert(55);
+      arr.insert(22);
+      arr.insert(88);
+      arr.insert(11);
+      arr.insert(00);
+      arr.insert(66);
+      arr.insert(33);
+      arr.insert(32);
+
+
+      arr.display();
+
+      arr.insertionSort();
+      arr.display();
+
+      Console.ReadKey();
+    }
+  }
+}
