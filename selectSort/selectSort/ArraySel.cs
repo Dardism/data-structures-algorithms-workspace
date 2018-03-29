4b4cd8cf303efa243bdfa84e9bb24f6938f28cdb
Dardism <dardismm@gmail.com>
2018-03-29 13:58:21 -0400

    completed selection sort

---
+++ selectSort/selectSort/ArraySel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selectSort {
+  class ArraySel {
+    private long[] a;
+    private int nElems; //number of elements
+
+    public ArraySel(int max) {
+      a = new long[max];
+      nElems = 0;
+    }
+    //***********************************
+    public void insert(long value) {
+      a[nElems] = value;
+      nElems++;
+    }
+    //***********************************
+    public void display() {
+      for (int j = 0; j < nElems; j++) {
+        Console.Write(a[j] + "  ");
+      }
+      Console.WriteLine("");
+    }
+    //***********************************
+    public void selectionSort() {
+      int inner;
+      int outer;
+      int min;
+
+      for (outer = 0; outer < nElems - 1; outer++) { //outer loop
+        min = outer;                            //minimum
+        for (inner = outer + 1; inner < nElems; inner++) { //inner loop
+          if (a[inner] < a[min]) {  //if min is greater, we have a new min
+            min = inner;
+          }
+        }
+        swap(outer, min);
+      }
+    }
+    //***********************************
+    public void swap(int one, int two) {
+      long temp = a[one];
+      a[one] = a[two];
+      a[two] = temp;
+    }
+  }
+}
---
+++ selectSort/selectSort/SelectSortApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selectSort {
+  class SelectSortApp {
+    static void Main(string[] args) {
+      int maxSize = 100; //array size
+      ArraySel arr = new ArraySel(maxSize);
+
+      arr.insert(77);
+      arr.insert(99);
+      arr.insert(44);
+      arr.insert(55);
+      arr.insert(22);
+      arr.insert(88);
+      arr.insert(11);
+      arr.insert(00);
+      arr.insert(66);
+      arr.insert(33);
+      arr.insert(32);
+
+
+      arr.display();
+
+      arr.selectionSort();
+      arr.display();
+
+      Console.ReadKey();
+
+    }
+  }
+}
