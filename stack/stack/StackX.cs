e338094476f242d5e402430226bb93497e9c8ee5
Dardism <dardismm@gmail.com>
2018-03-30 11:31:56 -0400

    updated StackX class to incorporate methods/functions for manipulating a stack object

--- stack/stack/StackX.cs
+++ stack/stack/StackX.cs
@@ -6,5 +6,34 @@ using System.Threading.Tasks;
 
 namespace stack {
   class StackX {
+    private int maxSize;
+    private long[] stackArray;
+    private int top;
+
+    public StackX(int s) {
+      maxSize = s;
+      stackArray = new long[maxSize];
+      top = -1;
+    }
+
+    public void push(long j) {
+      stackArray[++top] = j;
+    }
+
+    public long pop() {
+      return stackArray[top--];
+    }
+
+    public long peek() {
+      return stackArray[top];
+    }
+
+    public bool isEmpty() {
+      return (top == - 1);
+    }
+
+    public bool isFull() {
+      return (top == maxSize - 1);
+    }
   }
 }
